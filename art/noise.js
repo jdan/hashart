08ec390dc52a6b9dc57337bfb8a7588c3e7d23e2
Jordan Scales <scalesjordan@gmail.com>
2021-06-17 16:54:19 -0400

    make s more reliable

--- art/noise.js
+++ art/noise.js
@@ -8,27 +8,31 @@ class Noise extends Art {
     });
     this.filename = "noise.js";
     this.created = "09 Jun 2021";
-
-    this.s = 12;
   }
 
   getDescription() {
     return `
-      Divide the canvas into ${this.s}px by ${this.s}px squares and color square
-      <code>(x, y)</code> a shade of gray (0-255) using the formula:
+      Divide the canvas into squares and color square <code>(x, y)</code>
+      a shade of gray (0-255) using the formula:
 
       Math.abs(Math.sin(test * x * y)) * 256
     `;
   }
 
+  getSize(ctx) {
+    return _(12, ctx.canvas.width);
+  }
+
   draw(ctx, { test }) {
-    for (let x = 0; x < ctx.canvas.width; x += this.s) {
-      for (let y = 0; y < ctx.canvas.height; y += this.s) {
+    const s = this.getSize(ctx);
+
+    for (let x = 0; x < ctx.canvas.width; x += s) {
+      for (let y = 0; y < ctx.canvas.height; y += s) {
         const g = Math.floor(
-          Math.abs(Math.sin(test * (x / this.s + 1) * (y / this.s + 1))) * 256
+          Math.abs(Math.sin(test * (x / s + 1) * (y / s + 1))) * 256
         );
         ctx.fillStyle = `rgb(${g}, ${g}, ${g})`;
-        ctx.fillRect(x, y, this.s, this.s);
+        ctx.fillRect(x, y, s, s);
       }
     }
   }
