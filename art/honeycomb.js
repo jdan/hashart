cbb293c9958aa0b9722504dd3871603895df5aaf
Jordan Scales <scalesjordan@gmail.com>
2022-02-13 16:23:09 -0500

    description of the paths

--- art/honeycomb.js
+++ art/honeycomb.js
@@ -4,16 +4,22 @@ const { _, project } = require("./util.js");
 class HoneyComb extends Art {
   constructor() {
     super({
-      patterns: 22,
+      paths: 19,
     });
 
     this.filename = "honeycomb.js";
     this.created = "13 Feb 2022";
   }
 
-  getDescription() {
+  getDescription({ pathsBuffer }) {
     return `
-			TBA
+			Arrange ${pathsBuffer.length} hexagons into one larger hexagon. For
+      each of the ${pathsBuffer.length} bytes in the buffer: If the byte is
+      greater than <code>216</code>, do nothing. Otherwise, use the byte value
+      to generate two numbers <code>a, b</code> between 0 and 5.
+
+      Draw a path connecting side <code>a</code> to side <code>b</code>, or a
+      dead end if they are equal.
     `;
   }
 
@@ -35,7 +41,7 @@ class HoneyComb extends Art {
     }
     ctx.stroke();
 
-    const [a, b] = [Math.floor((pattern / 256) * 6), pattern % 6];
+    const [a, b] = [Math.floor(pattern / 36), pattern % 6];
 
     const gap = sideLength / 4;
     const l = (sideLength * Math.sqrt(3)) / 2;
@@ -114,7 +120,7 @@ class HoneyComb extends Art {
     }
   }
 
-  draw(ctx, { patternsBuffer }) {
+  draw(ctx, { pathsBuffer }) {
     const w = ctx.canvas.width;
     const h = ctx.canvas.height;
     const s = Math.max(w, h);
@@ -146,7 +152,9 @@ class HoneyComb extends Art {
     ].forEach(([a, b], idx) => {
       const x = w / 2 - Math.sqrt(3) * sideLength * (b / 2 + a);
       const y = h / 2 - (3 / 2) * sideLength * b;
-      this.drawHexagon(ctx, patternsBuffer[idx], x, y, sideLength);
+      if (pathsBuffer[idx] < 216) {
+        this.drawHexagon(ctx, pathsBuffer[idx], x, y, sideLength);
+      }
     });
   }
 }
